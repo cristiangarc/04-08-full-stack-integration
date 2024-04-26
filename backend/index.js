e50ded451b2feacb738da70f4e0f7ecdcab812c9
Cristian Garcia <crisg@sas.upenn.edu>
2024-04-26 17:00:53 -0400

    updated the func that provides a new id for each new task, to provide unique ids

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-let tasks = [ {id: 1, title: 'Laundry', description: 'wash, dry and fold laundry', status: "no" } ];
-// let tasks = [];
+let tasks = [{ id: 1, title: 'Laundry', description: 'wash, dry and fold clothes', status: "not yet completed" }];
+
+const getNewId = () => {
+    const ids = tasks.map(task => task.id);
+    let newId = tasks.length + 1;
+    while(true) {
+        if (!ids.includes(newId)) return newId;
+        newId++;
+    }
+}
 
 app.get('/tasks', (req, res) => {
     res.status(200).json(tasks);
@@ -15,7 +23,7 @@ app.post('/tasks', (req, res) => {
     // expect title, description, status in the request body
     const newTask = req.body;
     if (newTask.title && newTask.description && newTask.status) {
-        newTask.id = tasks.length + 1;
+        newTask.id = getNewId();
         tasks.push(newTask);
         res.status(201).json(newTask)
     } else {
